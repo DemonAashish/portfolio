6b3f148bd3d503e454ce40b0f46b6b473799dab9
aashishupreti <aashishupreti111@gmail.com>
2021-04-13 20:05:44 +0545

    arko error bro

--- src/components/About.js
+++ src/components/About.js
@@ -37,7 +37,7 @@ if(!author) return <div>Loading....</div>;
 
     return (
         <main className="relative">
-          
+          <img src={halo} alt="halo" className="absolute w-full"/>
             <div className="p-10 lg:pt-48 container mx-auto relative">
                 <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20">
                     <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8 " alt={author.name}/>
